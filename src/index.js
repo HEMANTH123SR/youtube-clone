57781725e2a8c55ab53de19761caa716231969b0
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-01-03 09:37:26 +0530

    feat: connection to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./env"
+});
+import express from "express";
+import {connectDB} from "./db/index.js"
+
+const app = express();
+
